48b0837e616fe5ec3d797fa6ed22666a1cb3ce4d
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2024-02-25 16:43:39 +0200

    Added JoinT and JoinCT commands 🫡

--- swiftly_movecommands/swiftly_movecommands.lua
+++ swiftly_movecommands/swiftly_movecommands.lua
@@ -70,16 +70,39 @@ commands:Register("t", function(playerid, args, argsCount, silent)
     end
 end)
 
+commands:Register("joint", function(playerid, args, argsCount, silent)
+    local player = GetPlayer(playerid)
+    if not player then return end
+
+    local playername = player:GetName()
+    local team = player:team():Get()
+    
+    if team == TEAM_T then return player:SendMsg(MessageType.Chat, FetchTranslation("movecommands.YouAlreadyT")) end
+    playermanager:SendMsg(MessageType.Chat, string.format(FetchTranslation("movecommands.YouMovedT"), playername))
+    player:team():Set(TEAM_T)
+end)
+
+commands:Register("joinct", function(playerid, args, argsCount, silent)
+    local player = GetPlayer(playerid)
+    if not player then return end
+
+    local playername = player:GetName()
+    local team = player:team():Get()
+    
+    if team == TEAM_CT then return player:SendMsg(MessageType.Chat, FetchTranslation("movecommands.YouAlreadyCT")) end
+    playermanager:SendMsg(MessageType.Chat, string.format(FetchTranslation("movecommands.YouMovedCT"), playername))
+    player:team():Set(TEAM_CT)
+end)
+
 commands:Register("afk", function(playerid, args, argsCount, silent)
     local player = GetPlayer(playerid)
     if not player then return end
 
-    local IsAdmin = exports["swiftly_admins"]:CallExport("HasFlags", playerid, "b")
     local playername = player:GetName()
     local team = player:team():Get()
     
-    if team == TEAM_SPECTATOR then return player:SendMsg(MessageType.Chat, FetchTranslation("movecommands.PlayerAlreadyAFK")) end
-    playermanager:SendMsg(MessageType.Chat, string.format(FetchTranslation("movecommands.MovedAFK"), playername))
+    if team == TEAM_SPECTATOR then return player:SendMsg(MessageType.Chat, FetchTranslation("movecommands.YouAlreadyAFK")) end
+    playermanager:SendMsg(MessageType.Chat, string.format(FetchTranslation("movecommands.YouMovedAFK"), playername))
     player:team():Set(TEAM_SPECTATOR)
 end)
 
